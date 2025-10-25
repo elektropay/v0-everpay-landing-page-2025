ef319cf4295562e4385b4d04c78af3734ea7e318
Elektropay <elektropay.commerce@gmail.com>
2025-10-25 13:36:11 -0500

    Update page.tsx

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,8 +1,11 @@
 "use client"
+import { SiteHeader } from "@/components/site-header"
+import { SiteFooter } from "@/components/site-footer"
 
 export default function Terms() {
   return (
-    <div>
+    <div className="min-h-screen bg-white">
+      <SiteHeader />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
           <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-100 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -71,6 +74,7 @@ export default function Terms() {
           </div>
         </div>
       </main>
+     <SiteFooter />
     </div>
   )
 }
